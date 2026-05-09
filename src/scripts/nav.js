2be77fa1b7508a1e7b4dfc5b634fe7085dd73149
ethancoote <realethancoote@gmail.com>
2026-05-09 06:57:56 -0400

    first 2 sections

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,5 +1,6 @@
 const navButton = document.querySelector('.nav-button');
 const nav = document.querySelector('.mobile-nav');
+const header = document.querySelector('.mobile-header');
 
 let isMenuOpen = false;
 navButton.addEventListener('click', () => {
@@ -14,7 +15,7 @@ function openMenu () {
     isMenuOpen = true;
     nav.style.opacity = 100;
     nav.inert = false;
-    nav.style.zIndex = "10";
+    header.style.zIndex = 10;
     document.body.style.overflow = "hidden";
     navButton.children[0].src = '/images/close.svg';
 }
@@ -23,7 +24,7 @@ function closeMenu() {
     isMenuOpen = false;
     nav.style.opacity = 0;
     nav.inert = true;
-    nav.style.zIndex = "-1";
+    header.style.zIndex = -1;
     document.body.style.overflow = "auto";
     navButton.children[0].src = '/images/open.svg';
 }
